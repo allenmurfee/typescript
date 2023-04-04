177bd71e80db1fb6f1b8b04ff24ab8a863866933
allenmurfee <allenmurfee123@gmail.com>
2023-04-04 10:35:42 -0400

    working on types

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ if (age < 50) {
     age+=10;
     console.log(age)
 }
+
+//Don't always have to annotate, TS compiler can infer that "sales" is a number
+let sales = 123_456_789;
+let course: string = "TypeScript";
+let isPublished: boolean = true;
+//Don't initialize, TS will assume is of type "any"
+let level;
