5d91b6d8daeeaa3c55db6fb8686c102ad8bc0c7c
allenmurfee <allenmurfee123@gmail.com>
2023-04-04 10:45:05 -0400

    methods autofill

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-console.log("Testing compiling")
+console.log("Testing compiling");
 
 //Colon then variable type annotates it
-let age: number = 20
+let age: number = 20;
 
 if (age < 50) {
-    age+=10;
-    console.log(age)
+  age += 10;
+  console.log(age);
 }
 
 //Don't always have to annotate, TS compiler can infer that "sales" is a number
@@ -14,19 +14,23 @@ let course: string = "TypeScript";
 let isPublished: boolean = true;
 //Don't initialize, TS will assume is of type "any"
 
-
 //"Any" Type
 //This removes the point of TypeScript though, so try to avoid using "any"
 let level;
 
 //This will throw error
 function render(document) {
-    console.log(document)
+  console.log(document);
 }
 
 //Declared as any
 function renderTwo(document: any) {
-    console.log(document)
+  console.log(document);
 }
 
-let numbers: number[] = [1,2,3]
+let numbers: number[] = [1, 2, 3];
+
+let numberArr: number[] = [];
+numberArr[0] = 1;
+//When doing "num." it will auto fill all number methods because TypeScript knows that "num" is a number.
+numberArr.forEach (num => num.toString())
